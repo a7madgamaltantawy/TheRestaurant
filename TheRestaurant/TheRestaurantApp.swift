6ee674c826716535886fcb90909bde0af3b86d98
ahmed tantawy <a7mad_gamal@live.com>
2021-03-15 17:09:48 +0200

    Initial Commit

---
+++ TheRestaurant/TheRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheRestaurantApp.swift
+//  TheRestaurant
+//
+//  Created by Ahmed Tantawy on 15/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct TheRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
