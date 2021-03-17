271a3550020248db97db3771c878cccf204c1687
ahmed tantawy <a7mad_gamal@live.com>
2021-03-17 19:12:49 +0200

    some polishing ti item row

---
+++ TheRestaurant/ItemRow.swift
@@ -0,0 +1,47 @@
+//
+//  ItemRow.swift
+//  TheRestaurant
+//
+//  Created by Ahmed Tantawy on 16/03/2021.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    
+    let item : MenuItem
+    
+    let  colors : [String : Color] = ["D":.purple, "G":.black , "N":.red , "S":.blue , "V":.green ] 
+    
+    
+    
+    var body: some View {
+        HStack{
+        Image(item.thumbnailImage)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.gray , lineWidth: 2))
+            
+            VStack(alignment : .leading){
+                Text(item.name)
+            .font(.headline)
+            Text("$\(item.price)")
+            }
+            Spacer()
+            ForEach(item.restrictions , id : \.self){restriction in
+                Text(restriction)
+                    .font(.footnote)
+                .fontWeight(.black)
+                .padding(4)
+                   // .background(DefaultMenuStyle)
+                .clipShape(Circle())
+                .foregroundColor(.blue)
+            }
+    }
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow(item: MenuItem.example)
+    }
+}
